dce24f646ed642712baf7011d75c663709f8157e
Muhammad Haseeb <haseebmuhammad578@gmail.com>
2026-06-21 16:50:26 +0500

    delete

--- Food App/src/theme/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './colors';
-export * from './typography';
-export * from './spacing';
-export * from './radii';
-export * from './elevation';
-export * from './tokens';
-export { ThemeProvider, ThemeContext, type ThemeContextValue } from './ThemeProvider';
-export { useTheme } from './useTheme';
-export { useThemedStyles } from './useThemedStyles';
